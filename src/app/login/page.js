d458a4b1fb9932577cbbb2b1619894c53de0ba55
Fed30 <136997447+Fed30@users.noreply.github.com>
2024-03-11 12:25:47 +0000

    login page update11/03/24
    alternative login options

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,4 +1,6 @@
-import Image from 'next/image';
+
+import Image from 'next/image'
+import Link from 'next/link'
 
 
 export default function Home() {
@@ -71,23 +73,20 @@ export default function Home() {
           
             
             <div className="loginAlternativeContainer">
-              <button
-                type="submit"
-                className="loginButtonFacebook"
-              >
-                
-                SIGN IN WITH FACEBOOK
-              </button>
+            <button type="button" class="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 me-2 mb-2">
+                <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
+                <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd"/>
+                </svg>
+                Sign in with Facebook
+                </button>
             </div>
             <div className="loginAlternativeContainer">
-              <button
-                type="submit"
-                className="loginButtonGoogle"
-              >
-                
-                  SIGN IN WITH GOOGLE
-                
-              </button>
+            <button type="button" class="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2">
+              <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
+              <path fill-rule="evenodd" d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z" clip-rule="evenodd"/>
+              </svg>
+              Sign in with Google
+            </button>
             </div>
           </form>
         </div>
@@ -115,7 +114,9 @@ export default function Home() {
                 type="submit"
                 className="registerButton"
               >
-                REGISTER
+                <Link href="/register" >
+                            REGISTER           
+                        </Link>
               </button>
             </div>
 
