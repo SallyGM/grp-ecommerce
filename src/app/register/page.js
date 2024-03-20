d63ea5c4d33bf2297df22b75f8655753daa8fa3e
Andrea Canestrari <andrea_4991@hotmail.it>
2024-03-20 15:05:00 +0000

    Update page.js

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className='grid grid-rows-2 grid-cols-1 display: flex justify-content: center gap-6 row-span-1 row-end-2'>
     <Card className="justify-self-center h-auto w-2/3 my-6 m-2.5">
+<<<<<<< Updated upstream
       
       <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white self-center">
         PERSONAL INFORMATION
@@ -25,6 +26,115 @@ export default function Home() {
           </div>
         </div>
       </div>
+=======
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white self-center">
+        PERSONAL INFORMATION
+      </h5>
+
+      <div class="mt-10 grid grid-cols-2 gap-x-6 gap-y-8 sm:grid-cols-6">
+        <div class="sm:col-span-3">
+          <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">First name</label>
+          <div class="mt-2">
+            <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          </div>
+        </div>
+
+        <div class="sm:col-span-3">
+          <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">First name</label>
+          <div class="mt-2">
+            <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          </div>
+        </div>
+      </div>
+
+      
+
+
+
+            
+
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form className="space-y-6" action="#" method="POST">
+        <div>
+          <label htmlFor="email" >
+            Email address
+          </label>
+          <div className="mt-2">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              placeholder='Mary.dickson@gmail.com'
+              required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <div className="flex items-center justify-between">
+            <label htmlFor="password" >
+              Password
+            </label>
+            </div>
+          </div>
+          <div class="relative">
+          <div className="mt-2">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              placeholder="•••••••••"
+              required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+        <div className="text-sm">
+              <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                Forgot password?
+              </a>
+            </div>
+
+        <div className="loginButtonContainer">
+          <Button
+            type="submit"
+            className="w-52 bg-green-400"
+          >
+            LOGIN
+          </Button>
+        </div>
+
+        <div class="grid-containerDivider">
+          <div class="grid-div1"><hr></hr></div>
+          <div class="grid-div2">OR</div>
+          <div class="grid-div3"><hr></hr></div> 
+        </div>
+        
+      
+        
+        <div className="loginAlternativeContainer">
+        <Button type="button" class="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 me-2 mb-2">
+            <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
+            <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd"/>
+            </svg>
+            Sign in with Facebook
+            </Button>
+        </div>
+        <div className="loginAlternativeContainer">
+        <Button type="button" class="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2">
+          <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
+          <path fill-rule="evenodd" d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z" clip-rule="evenodd"/>
+          </svg>
+          Sign in with Google
+        </Button>
+        </div>
+      </form>
+        </div>
+>>>>>>> Stashed changes
 
     </Card>
 
