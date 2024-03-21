94d8d6701240467bd876096a2aa3cc2fc0bf6eeb
Andrea Canestrari <andrea_4991@hotmail.it>
2024-03-21 19:13:39 +0000

    Update page.js

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -3,49 +3,69 @@ import { Card, Button } from 'flowbite-react';
 
 export default function Home() {
   return (
-    <div className='grid grid-rows-1 grid-cols-2 display: flex justify-content: center gap-6 row-span-1 row-end-2 bg-blue-800 ' >
+    <div className='grid grid-rows-2 grid-cols-1 p-8 display: flex justify-content: center gap-6 bg-blue-800 ' >
 
 
       {/*Personal information card*/}
-      <Card className="justify-self-end h-auto  w-2/3 my-6 bg-blue-900 border-blue-900">
+      <Card className="justify-self-center flex-wrap h-auto w-4/5 my-6 bg-blue-900 border-blue-900">
 
-        <h1 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white self-center text-white font-mono">Personal Information</h1>
-        <br/><br/>
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white self-center text-white font-mono">PERSONAL INFORMATION</h1>
+        <br/>
 
-        <form>
-          <label for="firstName">First Name</label>
-          <input type="text" id="firstName"></input>
+        <form className="grid grid-rows-1 grid-cols-2 display: flex gap-6 text-white font-mono">
+          <div>
+            <label for="firstName">First Name</label>
+            <input className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            type="text" id="firstName"  name="firstName" required></input>
+          </div>
+          
+          <div>
+            <label for="lastName">Last Name</label>
+            <input className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            type="text" id="lastName"  name="lastName" required></input>
+          </div>
 
-          <label for="lastName">Last Name</label>
-          <input type="text" id="lastName"></input>
         </form>
-        <br/><br/>
+
       </Card>
 
 
       {/*Sign in information card*/}
-      <Card className="justify-self-end h-auto  w-2/3 my-6 bg-blue-900 border-blue-900">
+      <Card className="justify-self-center h-auto w-4/5 my-6 bg-blue-900 border-blue-900">
         
-        <h1 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white self-center text-white font-mono">Sign in Information</h1>
-        <br/><br/>
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white self-center text-white font-mono">SIGN-IN INFORMATION</h1>
+        <br/>
 
-        <form>
-          <label for="email">Email</label>
-          <input type="email" id="email"></input>
+        <form className="grid grid-rows-2 grid-cols-2 gap-6 display: flex text-white font-mono">
 
-          <label for="confirmEmail">Confirm email</label>
-          <input type="email" id="confirmEmail"></input>
+          <div>
+            <label for="email">Email</label>
+            <input className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            type="email" id="email" name="email" required></input>
+          </div>
 
-          <label for="password">Password</label>
-          <input type="password" id="password"></input>
+          <div>
+            <label for="password">Password</label>
+            <input className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            type="password" id="password" name="password" required></input>
+          </div>          
 
-          <label for="confirmPassword">Confirm Password</label>
-          <input type="password" id="confirmPassword"></input>
+          <div>
+            <label for="confirmEmail">Confirm email</label>
+            <input className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            type="email" id="confirmEmail" name="confirmEmail" required></input>
+          </div>
+
+          <div>
+            <label for="confirmPassword">Confirm Password</label>
+            <input className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            type="password" id="confirmPassword" name="confirmPassword" required></input>            
+          </div>
+
         </form>
       </Card>
-    
-      
 
+      <Button className="w-4/12 justify-self-center bg-green-400" type="submit">CREATE ACCOUNT</Button>
 
     </div>
   )}
