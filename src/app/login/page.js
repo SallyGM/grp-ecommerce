17db60809f4b4387073b24d14d63e44e5ccb6f8d
Fed30 <136997447+Fed30@users.noreply.github.com>
2024-03-22 18:00:16 +0000

    login_register_pages_update.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -5,6 +5,7 @@ import { Card, Button } from 'flowbite-react';
 import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import React from 'react';
+import { Divider } from '@nextui-org/react';
 
 
 export default function Home() {
@@ -54,88 +55,78 @@ export default function Home() {
   };
 
   return (
-    <div className='grid grid-rows-1 grid-cols-2 display: flex justify-content: center gap-6 row-span-1 row-end-2 bg-blue-800 ' > 
+    <div className='grid grid-rows-1 grid-cols-2 display: flex justify-content: center gap-6 row-span-1 bg-blue-800 ' > 
       <Card className="justify-self-end h-auto  w-2/3 my-6 bg-blue-900 border-blue-900"  >
-        <h1 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white self-center text-white font-mono ">
-          LOGIN
-        </h1>
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <h1 className="self-center text-4xl font-bold text-white font-mono ">LOGIN</h1>
+        <div className="self-center sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6 text-white font-mono" action="#" method="POST onSubmit={handleSubmit}" >
+
             <div>
               <label htmlFor="email">Email address</label>
-              <input className="block w-full mt-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              <input className="block w-full mt-2 rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               type="email" id="email" name="email" required value={email} onChange={handleEmailChange}/>
                 {emailError && <span style={{ color: 'red', fontSize: '14px' }}>{emailError}</span>}  
             </div>
 
             <div>
               <label htmlFor="password">Password</label>
-              <input className="block w-full mt-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              <input className="block w-full mt-2 my-2.5 rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               type="password" id="password" name="password" required value={password} onChange={handlePasswordChange}/>
                 {passwordError && <span style={{ color: 'red', fontSize: '14px' }}>{passwordError}</span>}
             </div>
           </form>
-         
-          <a href="#" className="text-sm font-semibold text-indigo-600 hover:text-indigo-500 text-white">Forgot password?</a>
-
-          <div className='flex place-content-end'>
-            <Button className="justify-self-end w-52 bg-green-400" type="submit">LOGIN</Button>
+          <a href="#" className="text-sm font-semibold text-indigo-600  hover:text-indigo-500 text-white">Forgot password?</a>
+          <Button className="justify-self-center w-full mt-7 bg-green-400" type="submit">LOGIN</Button>
           </div>
 
-          {/*Divider, to fix with Tailwind */} 
-          <div class="grid-containerDivider">
-            <div class="grid-div1"><hr></hr></div>
-            <div class="grid-div2 font-mono">OR</div>
-            <div class="grid-div3"><hr></hr></div> 
+          {/*Divider between login options*/} 
+          <div class="inline-flex mt-6">
+            <Divider className="self-center  w-40 m-3"></Divider>
+            <a className="justify-self-center text-white m-3">OR</a>
+            <Divider className="self-center w-40 m-3"></Divider>
           </div>
           
           {/*Facebook sign in button*/}
-          <div className='flex place-content-center'>
-            <Button type="button" class="text-white bg-[#3B82F6] hover:bg-[#1A68DC]/90 focus:ring-4 focus:outline-none focus:ring-[#1A68DC]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#1A68DC]/55 me-2 mb-2">
-              <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
-                <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd"/>
-              </svg>
-              Sign in with Facebook
-            </Button>
-          </div>
+          <Button className="inline-flex bg-blue-900 text-white w-72 self-center">
+            <svg className="w-6 h-6 text-white m-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path d ="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0014.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"/>
+            </svg>
+            Sign in with Facebook
+          </Button>
 
           {/*Google sign in button*/}
-          <div className='flex place-content-center'>
-            <Button type="button" class="text-white bg-[#C7573E] hover:bg-[#EF4444]/90 focus:ring-4 focus:outline-none focus:ring-[#EF4444]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#EF4444]/55 me-2 mb-2">
-              <svg class="w-8 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
-              <path fill-rule="evenodd" d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z" clip-rule="evenodd"/>
-              </svg>
-              Sign in with Google
-            </Button>
-          </div>
-        </div>
-
+          <Button className="inline-flex bg-red-700 text-white w-72 self-center">
+            <svg className="w-6 h-6 text-white m-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path d ="M6 12a6 6 0 0011.659 2H12v-4h9.805v4H21.8c-.927 4.564-4.962 8-9.8 8-5.523 0-10-4.477-10-10S6.477 2 12 2a9.99 9.99 0 018.282 4.393l-3.278 2.295A6 6 0 006 12z"/>
+            </svg>
+            Sign in with Google
+          </Button>
+        
       </Card>
 
       <Card className=" justify-self-start h-auto  w-2/3 my-6 bg-blue-900 border-blue-900" >
-        <h1 className="text-4xl font-bold text-white font-mono ">REGISTER</h1>
+        <h1 className="self-center text-4xl font-bold text-white font-mono mt-8 ">REGISTER</h1>
 
         <p className="text-white text-center font-mono my-8">SIMPLY CLICK ON THE REGISTER BUTTON AND BECOME PART OF A HUGE ONLINE COMUNITY</p>
         
-        <div class="grid-benefit">
-          <svg className="text-white" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-tags" viewBox="0 0 16 16">
+        <div class="inline-flex mt-6 place-content-evenly">
+          <svg className="text-white mr-6" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" class="bi bi-tags" viewBox="0 0 16 16">
             <path d="M3 2v4.586l7 7L14.586 9l-7-7zM2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586z"/>
             <path d="M5.5 5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1m0 1a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3M1 7.086a1 1 0 0 0 .293.707L8.75 15.25l-.043.043a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 0 7.586V3a1 1 0 0 1 1-1z"/>
           </svg>
-          
           <a className="font-mono text-white">RECEIVE DISCOUNT AND BENEFIT</a>
-          
-          <svg className="text-white" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-globe" viewBox="0 0 16 16">
+        </div> 
+        <div class="inline-flex mt-9 place-content-evenly">
+          <svg className="text-white mr-6" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" class="bi bi-globe" viewBox="0 0 16 16">
             <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m7.5-6.923c-.67.204-1.335.82-1.887 1.855A8 8 0 0 0 5.145 4H7.5zM4.09 4a9.3 9.3 0 0 1 .64-1.539 7 7 0 0 1 .597-.933A7.03 7.03 0 0 0 2.255 4zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a7 7 0 0 0-.656 2.5zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5zM8.5 5v2.5h2.99a12.5 12.5 0 0 0-.337-2.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5zM5.145 12q.208.58.468 1.068c.552 1.035 1.218 1.65 1.887 1.855V12zm.182 2.472a7 7 0 0 1-.597-.933A9.3 9.3 0 0 1 4.09 12H2.255a7 7 0 0 0 3.072 2.472M3.82 11a13.7 13.7 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5zm6.853 3.472A7 7 0 0 0 13.745 12H11.91a9.3 9.3 0 0 1-.64 1.539 7 7 0 0 1-.597.933M8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855q.26-.487.468-1.068zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.7 13.7 0 0 1-.312 2.5m2.802-3.5a7 7 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7 7 0 0 0-3.072-2.472c.218.284.418.598.597.933M10.855 4a8 8 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4z"/>
           </svg>
-        </div> 
-          
-        <a className="text-white font-mono">BE PART OF A COMMUNITY</a>
-        
-        <Button type="submit" className=" flex place-content-center w-72 my-28 bg-green-400">
+          <a className="text-white font-mono">BE PART OF A COMMUNITY</a>
+        </div>
+
+        <Button type="submit" className=" self-center w-72 my-28 bg-green-400">
           <Link href="/register">REGISTER</Link>
         </Button>
+
       </Card>
 
     </div>
