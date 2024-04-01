8f0aa78e32a75606828571c95acaec24c2e3bbf1
Andrea Canestrari <andrea_4991@hotmail.it>
2024-04-01 18:23:10 +0100

    Update page.js

--- src/app/register3/page.js
+++ src/app/register3/page.js
@@ -4,6 +4,7 @@ import { Card, Button } from 'flowbite-react';
 
 export default function Home() {
   return (
+    
     <div className='grid grid-rows-1 grid-cols-1 p-8 flex justify-content-center bg-dark-night'>
 
       {/*Status bar */}
