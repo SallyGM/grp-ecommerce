0375b969f2fe709cfbe930e0c0aa0bdab8fa9e17
Fed30 <136997447+Fed30@users.noreply.github.com>
2024-04-13 22:09:33 +0100

    Profile Order
    Data retrieval working

--- src/app/profile/order/page.js
+++ src/app/profile/order/page.js
@@ -9,85 +9,61 @@ import { useAuth } from '@/app/context/AuthContext.js';
 import VpnKeyIcon from '@mui/icons-material/VpnKey';
 import { Tooltip } from 'flowbite-react';
 import {RateReview} from '@mui/icons-material'; // Import icons
+import { useProductContext } from '@/app/context/ProductContext.js';
 
 
 
 export default function MyOrders() {
-
-    // Firebase information retrival function here
-
+    const { products } = useProductContext();
+    const { currentUser } = useAuth();
     const [OrderDetails, setOrderDetails] = useState([]);
 
-
-    // Get the currently signed-in user
-    const { currentUser } = useAuth()
-
     useEffect(() => {
-        const userId = currentUser.uid;
-        if (!userId) {
-            console.log("No current user logged in");
-            return;
-        }
+        if (currentUser && currentUser.uid) { // Ensure currentUser and currentUser.id are valid
+            const userId = currentUser.uid;
+            const ordersRef = ref(database, 'Order');
+            const userOrdersQuery = query(ordersRef, orderByChild('userID'), equalTo(userId));
     
-        const orderDetailsRef = ref(database, "Order");
-        const orderQuery = query(orderDetailsRef, orderByChild('userID'), equalTo(userId));
+            get(userOrdersQuery).then((snapshot) => {
+                if (snapshot.exists()) {
+                    const orders = Object.entries(snapshot.val()).map(([id, data]) => ({
+                        id,
+                        ...data,
+                    }));
     
-        get(orderQuery).then(async (snapshot) => {
-            if (snapshot.exists()) {
-                const orderDetailsArray = [];
-                const orders = snapshot.val();
-    
-                for (const orderId in orders) {
-                    const order = orders[orderId];
-                    const items = order.items; // Assuming 'items' contains item IDs and quantities
-                    const itemsArray = [];
-    
-                    for (const itemId in items) {
-                        // Fetch product details for each item from the 'Product' table
-                        const productRef = ref(database, `Product/${itemId}`);
-                        const productSnapshot = await get(productRef);
-                    
-                        console.log("Product Snapshot for item ID", itemId, ":", productSnapshot.val()); // Log product data); // Log product data
-                    
-                        if (productSnapshot.exists()) {
-                            const productData = productSnapshot.val();
-                            const item = {
-                                id: itemId,
-                                quantity: items[itemId],
-                               // price: productData.name,
-                               // name: productData.name,
-                                ...productData // Include product details
-                            };
-                            itemsArray.push(item);
-                        }
-                    }
-                    console.log("Items Array:", itemsArray); // Log items array
-    
-                    orderDetailsArray.push({
-                        id: orderId,
-                        items: itemsArray,
-                        ...order // Include other order details
-                    });
-                    console.log(orderDetailsArray);
+                    console.log("Orders:", orders); // Log orders to see if data is retrieved correctly
+                    setOrderDetails(orders);
+                } else {
+                    console.log("No orders found");
+                    setOrderDetails([]);
                 }
-    
-                setOrderDetails(orderDetailsArray);
-                console.log(OrderDetails);
-            } else {
-                console.log("No orders found for the current user");
-            }
-        }).catch((error) => {
-            console.error(error);
-        });
-    }, [currentUser]);
-    
+            }).catch((error) => {
+                console.error("Error fetching orders:", error);
+                setOrderDetails([]);
+            });
+        }
+    }, [currentUser, products]); // Include products in the dependency array
 
+    const getItemsForOrder = (orderId) => {
+        const order = OrderDetails.find(order => order.id === orderId);
+        if (order && typeof order.items === 'object') {
+            return Object.keys(order.items).map(itemId => {
+                const product = products.find(product => product.id === itemId);
+                return {
+                    product,
+                    quantity: order.items[itemId] 
+                };
+            });
+        }
+        return [];
+    };
+    
+    
     return (
         <div className='grid grid-rows-1 grid-cols-4 gap-x-20 row-start-1 row-end-2 col-start-1 col-end-3 bg-dark-night justify-items-center'> 
             <SubNavbar />
-            <div style={{ backgroundColor: 'transparent' }} className="justify-items-center h-auto w-full my-6 mr-12 mt-24 row-start-1 row-end-1 col-start-2 col-end-5">
-                <h5 className="justify-self-center text-center text-4xl mb-6 font-bold tracking-tight text-white font-mono"> MY ORDERED KEYS</h5>
-                <div className='grid grid-rows-3 flex-wrap m-s ml-10 mr-10'>
+            <div style={{ backgroundColor: 'transparent' }} className=" justify-items-center h-auto w-full my-6 mr-12 mt-24 bg-blue-900 border-blue-900 row-start-1 row-end-1 col-start-2 col-end-5 " >
+                <h5 className="justify-self-center text-center text-4xl mb-6 font-bold tracking-tight text-white font-mono" > MY ORDER KEYS</h5>
                     {OrderDetails.map((o) => (
                         <Card key={o.id} style={{ backgroundColor: 'transparent' }} className="flex h-auto w-full border-2 border-white mt-3">
                             <div className=' grid grid-cols-3 flex-wrap ml-3 mr-3 p-3' style={{ gridTemplateColumns: '1fr 1fr 1fr 1fr', justifyItems: 'start' }}>
@@ -102,22 +78,23 @@ export default function MyOrders() {
                                 <a className='tracking-tight dark:text-white text-white'>{"£ "+ parseFloat(o.price).toFixed(2)}</a>
                                 <a className='tracking-tight dark:text-white text-white'>{o.status}</a>
                             </div>
-                            <div className='border-b-2 border-white grid grid-cols-5 flex-wrap ml-3 mr-3 p-3' style={{ gridTemplateColumns: '1fr 1fr 1fr 1fr', justifyItems: 'start' }}>
+                            <div className='border-b border-gray-300 grid grid-cols-5 flex-wrap ml-3 mr-3 p-3' style={{ gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr', justifyItems: 'start' }}>
                                 <a className='tracking-tight dark:text-white text-white'>Product</a>
+                                <a className='tracking-tight dark:text-white text-white'></a>
                                 <a className='tracking-tight dark:text-white text-white'>Price</a>
                                 <a className='tracking-tight dark:text-white text-white'>Review</a>
                                 <a className='tracking-tight dark:text-white text-white'>Key</a>
                             </div>
                             {/* Display item details */}
-                            {o.items && Object.entries(o.items).map(([itemId, item]) => (
-                                <div key={itemId} className='grid grid-cols-7 items-center flex-wrap border-b-2 border-white ml-3 mr-3 p-3' style={{ gridTemplateColumns: '1fr 1fr 1fr 1fr', justifyItems: 'start' }}>    
-                                    {item && (
+                            {getItemsForOrder(o.id).map((item, index) => (
+                                <div key={index} className='grid grid-cols-7 items-center flex-wrap border-b border-gray-300 ml-3 mr-3 p-3' style={{ gridTemplateColumns: ' 1fr 1fr 1fr 1fr 1fr', justifyItems: 'start' }}>    
+                                    {item && item.product && (
                                         <Fragment>
-                                            {console.log('item: ',item)}
-                                            <div className="flex dark:text-white text-white font-mono">{item.name}</div>
-                                            {item.price !== undefined && (
-                                                <div className="flex text-center dark:text-white text-white font-mono">{'£ ' + item.price}</div>
-                                            )}
+                                            {console.log('item.product:', item.product)}
+                                            <img className="w-16 h-16 object-cover rounded-lg" src={item.product.images[1]} alt="Product Image"/>
+                                            <div className="flex dark:text-white text-white font-mono ml-3">{item.product.name}</div>
+                                            <div className="flex text-center dark:text-white text-white font-mono">{'£ ' + item.product.price}</div>
+                                           
                                             <Tooltip content='Review this product'>
                                                 <RateReview style={{ height: '20px', width: '20px', justifySelf: 'center', filter: 'brightness(0) invert(1)' }} />
                                             </Tooltip>
@@ -132,5 +109,4 @@ export default function MyOrders() {
                     ))}
                 </div>             
             </div>
-        </div>
-    );}
+ );}
