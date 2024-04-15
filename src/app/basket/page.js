4e8b64abdcf96ff7faba2b974d0f3f433bd429c0
Andrea Canestrari <andrea_4991@hotmail.it>
2024-04-16 00:05:45 +0100

    Update page.js

--- src/app/basket/page.js
+++ src/app/basket/page.js
@@ -295,7 +295,7 @@ export default function Home() {
               </div>
               <div className="relative z-0 w-full mb-5 group">
                 <label>Sort Code</label>
-                <InputMask ref={sortCode} onChange={handleSortCode} className="block w-full rounded-md py-1.5 px-1.5 mt-2 border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" mask="99 99 99" maskChar="" placeholder='Sort Code...' required/>  
+                <InputMask ref={sortCode} onChange={handleSortCode} className="block w-full rounded-md py-1.5 px-1.5 mt-2 border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" mask="99-99-99" maskChar="" placeholder='Sort Code...' required/>  
                 {sortCodeError && <span style={{ color: 'red', fontSize: '12px' }}>{sortCodeError}</span>}
               </div>
               <div className="grid md:grid-cols-2 md:gap-6">
