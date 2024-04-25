def033470e1b6bf1a08bb52f031c194c1b23e163
Andrea Canestrari <andrea_4991@hotmail.it>
2024-04-25 19:37:19 +0100

    BUG FIX (Add to favourites)

--- src/components/productDetails.js
+++ src/components/productDetails.js
@@ -47,7 +47,7 @@ export default function Page({ productIDParam }) {
     }, [favourites]);
 
     // add to the favourites
-    function handleClickOnFavourite( e){
+    function handleClickOnFavourite(e){
         // add to the list if the item is not there
         if(!favourite.some(item => item.id === productID)){
             addToFavourites(productID)
@@ -122,11 +122,11 @@ export default function Page({ productIDParam }) {
                                             <img src={product.images[0]} alt="Image" className=" object-contain rounded-lg" />
                                             {currentUser  ? (
                                                 (favourite.some(item => item.id === product.id) ? (
-                                                    <svg xmlns="http://www.w3.org/2000/svg" onClick={(e) => handleClickOnFavourite(p.id, e)} viewBox="0 0 24 24" fill="red" className="absolute cursor-pointer ml-2 top-4 w-7 h-7 hover:scale-125">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" onClick={(e) => handleClickOnFavourite(e)} viewBox="0 0 24 24" fill="red" className="absolute cursor-pointer ml-2 top-4 w-7 h-7 hover:scale-125">
                                                     <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
                                                     </svg>
                                                 ) : (
-                                                    <svg xmlns="http://www.w3.org/2000/svg" onClick={(e) => handleClickOnFavourite(p.id, e)} fill="#f7f7f7" viewBox="0 0 24 24" className="absolute cursor-pointer ml-2 top-4 w-7 h-7 hover:scale-125">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" onClick={(e) => handleClickOnFavourite(e)} fill="#f7f7f7" viewBox="0 0 24 24" className="absolute cursor-pointer ml-2 top-4 w-7 h-7 hover:scale-125">
                                                     <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                                                     </svg>
                                                 ))
